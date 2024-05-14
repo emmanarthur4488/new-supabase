d0743004509b46e392d3b8e52cda80f27f8dae25
emmanarthur4488 <arthurgodswill56@gmail.com>
2024-05-14 09:13:26 +0100

    first commit

---
+++ supabase/src/Componets/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+          <ul>
+              <li>Home</li>
+              <li>Contact</li>
+              <li>Update</li>
+          </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
